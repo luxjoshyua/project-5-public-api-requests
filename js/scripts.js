13e107a502c18107ec23be85378c93365a7086a0
luxjoshyua <josh.e.fielding@gmail.com>
2019-11-03 15:16:49 +1100

    got eventlistener working

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,12 @@ async function fetchRequest(url) {
 }
 
 const createUserCard = (data) => {
-
+  // loop through each user and populate the card with the fetched data
   data.forEach(user => {
+
     // Setup card in the DOM
     const cardContainerDiv = document.createElement("div");
     cardContainerDiv.classList.add("card");
-    
     gallery.appendChild(cardContainerDiv);
     // Set the card HMTL
     cardContainerDiv.innerHTML =
@@ -34,72 +34,99 @@ const createUserCard = (data) => {
           <p class="card-text">${user.email}</p>
           <p class="card-text cap">${user.location.city}</p>
       </div>
+    `
+
+
     
-    `
-    // Setup modal in the DOM
-    const modalContainer = document.createElement("div");
-    modalContainer.classList.add("modal-container");
-    document.body.appendChild(modalContainer);
-    // Set the modal HTML
-    modalContainer.innerHTML =
-      `
-      <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="${user.picture.medium}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">
-            ${user.name.first}
-            ${user.name.last}
-            </h3>
-            <p class="modal-text">
-            ${user.email}
-            </p>
-            <p class="modal-text cap">
-             ${user.location.city}
-            </p>
-            <hr>
-            <p class="modal-text">${user.cell}</p>
-            <p class="modal-text">
-            ${user.street.number}
-            ${user.street.name}
-            ${user.city}
-            ${user.postcode}
-            </p>
-            <p class="modal-text">Birthday: ${user.dob.date}</p>
-        </div>
-    `
   })
 }
 
 
-const modalEvents = (data) => {
-  const card = document.querySelectorAll(".card");
-  const modal = document.querySelectorAll(".modal");
-  // Setup event listener on the card
-  card.addEventListener('click', (e) => {
-    const event1 = event.target;
-    console.log(event1);
-    if (event1 === true) {
-      modal.style.display = 'block';
-      card.style.display = 'none';
-    } else {
-      modal.style.display = 'none';
-      card.style.display = 'flex';
-    }
-  })
-  // Setup event listener on the modal
-  modal.addEventListener('click', (e) => {
-    const event2 = event.target;
-    const close = modal.getElementById("modal-close-btn");
+// const createModalCard = (data) => {
 
-    if (event2 === close || close.innerHTML === 'X') {
-      modal.style.display = 'none';
-      card.style.display = 'flex';
+//   data.forEach(user => {
+
+//     // Setup modal in the DOM
+//     const modalContainer = document.createElement("div");
+//     modalContainer.classList.add("modal-container");
+//     document.body.appendChild(modalContainer);
+//     // Set the modal HTML
+//     modalContainer.innerHTML =
+//       `
+//       <div class="modal">
+//         <button type="button" id="modal-close-btn" class="modal-close-btn">
+//           <strong>X</strong>
+//         </button>
+//         <div class="modal-info-container">
+//             <img class="modal-img" src="${user.picture.medium}" alt="profile picture">
+//             <h3 id="name" class="modal-name cap">
+//             ${user.name.first}
+//             ${user.name.last}
+//             </h3>
+//             <p class="modal-text">
+//             ${user.email}
+//             </p>
+//             <p class="modal-text cap">
+//              ${user.location.city}
+//             </p>
+//             <hr>
+//             <p class="modal-text">${user.cell}</p>
+//             <p class="modal-text">
+//             ${user.street.number}
+//             ${user.street.name}
+//             ${user.city}
+//             ${user.postcode}
+//             </p>
+//             <p class="modal-text">Birthday: ${user.dob.date}</p>
+//         </div>
+//     `
+//   })
+// }
+
+
+const modalEvents = (data) => {
+
+// Setup event listener for card
+// desired behaviour: when clicked, the modal for that card opens, the modal is then closed by hitting the X
+const cards = document.querySelectorAll(".card");
+// const modal = document.querySelectorAll(".modal");
+// console.log(modal); 
+
+cards.forEach(function (card) {
+  // console.log(card);
+  // addEventListener can only be invoked on a single node at a time
+  card.addEventListener('click', (e) => {
+    const clickedCard = e.target;
+    // console.log(clickedCard);
+    if (clickedCard) {
+      //  modal.style.display = 'block';
+      console.log('card has been clicked, now show the modal!'); 
     } else {
-      modal.style.display = 'block';
-      card.style.display = 'none';
+      console.log('card hasn\'t been clicked, carry on!'); 
+      //  modal.style.display = 'none';
+      // clickedCard.style.display = 'flex';
     }
   })
+})
+
+
+
+
+
+
+// Setup event listener on the modal
+// modal.addEventListener('click', (e) => {
+//   const event2 = event.target;
+//   const close = modal.getElementById("modal-close-btn");
+
+//   if (event2 === close || close.innerHTML === 'X') {
+//     modal.style.display = 'none';
+//     card.style.display = 'flex';
+//   } else {
+//     modal.style.display = 'block';
+//     card.style.display = 'none';
+//   }
+// })
 }
 
 
@@ -107,6 +134,7 @@ const modalEvents = (data) => {
 
 fetchRequest(urlRequest)
   .then(createUserCard)
+  // .then(createModalCard)
   .then(modalEvents)
   .catch(error => {
     const errorPage = document.querySelector('.error');
@@ -120,24 +148,5 @@ fetchRequest(urlRequest)
    * === JUNK CODE ====
    * 
    * 
-// Setup gallery of 12 profiles you see on initial screen load
-// function createUserCard(data) {
 
-//   const card = data.map(card => `
-//     <div class="card">
-//     <div class="card-img-container">
-//         <img class="card-img" src="${card.picture.thumbnail}" alt="profile picture">
-//     </div>
-//     <div class="card-info-container">
-//         <h3 id="name" class="card-name cap" value="${data.results}>${card.name.first}${card.name.last}</h3>
-//         <p class="card-text">${card.email}</p>
-//         <p class="card-text cap">${card.location}</p>
-//     </div>
-//   </div>
-//   `)
-// }
-   * 
-   * 
-   * 
-   * 
    */
